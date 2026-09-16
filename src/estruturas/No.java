8b1b2af1325691b3ed9420bcb6b0a0774a0ccbd5
Nuno Nhantumbo <nhantumbonuno@gmail.com>
2026-09-16 16:27:36 +0200

    Implementação da Fila de pacientes

--- src/estruturas/No.java
+++ src/estruturas/No.java
@@ -1,1 +1,20 @@
+package estruturas;
 
+public class No{
+    private Paciente paciente;
+    private No proximo;
+    public No(Paciente paciente){
+        this.paciente = paciente;
+        this.proximo = null;
+    }
+    public Paciente getPaciente(){
+        return paciente;
+    }
+    public No getProximo(){
+        return proximo;
+
+    }
+    public void setProximo(No proximo){
+        this.proximo = proximo;
+    }
+}
